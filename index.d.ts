51b56e320fc58a62ee093661da089dc280e298d9
Dimitri Benin <BendingBender@users.noreply.github.com>
2019-04-11 04:08:08 +0000

    Require Node.js 8, add TypeScript definition (#1)

---
+++ index.d.ts
@@ -0,0 +1,22 @@
+/**
+Generate a random integer.
+
+@param minimumValue - Minimum integer to return. Default: `0`.
+@param maximumValue - Maximum integer to return. Default: `1`.
+@returns An integer from `minimumValue` to `maximumValue`.
+
+@example
+```
+import randomInt = require('random-int');
+
+randomInt(5);
+//=> 3
+
+randomInt(10, 100);
+//=> 54
+```
+*/
+declare function randomInt(maximumValue?: number): number;
+declare function randomInt(minimumValue: number, maximumValue: number): number;
+
+export = randomInt;
